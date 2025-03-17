9404dd3f401d36484614820b1604ba6feef3bfcf
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2025-03-17 12:03:14 -0300

    c

--- teoria y practico/Unidad 1/practico/ejercicio9/main.js
+++ teoria y practico/Unidad 1/practico/ejercicio9/main.js
@@ -1,50 +1,22 @@
 import {
-  formulario,
-  ValidarFormulario,
-  encabezado,
-  datos,
-  cargarDatos,
-  cargarTabla,
-  valorePorDefecto
-
+  intputBuscar,
+  lista,
+  msjGeneral,
+  validarBuscar,
+  crearLi,
+  baseDatos
 } from './variables.js';
 
-valorePorDefecto()
-cargarTabla()
 
-formulario.addEventListener("submit", (event) => {
-  event.preventDefault()
-  if (ValidarFormulario()) {
-    //formulario.submit() al formulario por el momento no lo voy a enviar hasta que entinda mas sobre node js
-
-    datos.push(cargarDatos())
-    cargarTabla()
-    formulario.reset()
-
+intputBuscar.addEventListener('input', () => {
+  //msjGeneral.innerHTML = intputBuscar.value; para ver lo que se escribe en el input
+  if (!validarBuscar()) {
+    return;
   }
+  const texto = intputBuscar.value.toLowerCase();
+  const sugerencias = baseDatos.filter(palabra => palabra.toLowerCase().startsWith(texto));
 
-});
+  crearLi([... new Set(sugerencias)]);
 
-encabezado.addEventListener("click", (event) => {
 
-  if (event.target.tagName === "TH") {
-    const idBoton = event.target.dataset.id;
-    //1 es año 2 suceso 3 inflacion
-    datos.sort((a, b) => {
-      if (idBoton == 1) {
-        return a.year - b.year
-      }
-      if (idBoton == 2) {
-        return a.suceso.localeCompare(b.suceso)
-      }
-      if (idBoton == 3) {
-        return a.inflacion - b.inflacion
-      }
-    })
-    cargarTabla()
-  }
 })
-
-
-
-
